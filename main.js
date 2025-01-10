16abcbc8b6da66a017ccd729892dc737b6787e74
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-10 11:51:37 +0100

    end: end code

--- main.js
+++ main.js
@@ -11,3 +11,19 @@
 //      - altrimenti se è solo multiplo di 3 -> stampo Fizz
 //      - altrimenti se è solo multiplo di 5 -> stampo Buzz
 //      - altrimenti stampo solo il num
+
+// code
+
+for (let i = 1; i <=100; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log(`FizzBuzz (${i})`)
+    } else if (i % 3 == 0) {
+        console.log(`Fizz (${i})`)
+    } else if (i % 5 == 0) {
+        console.log(`Buzz (${i})`)
+    } else {
+        console.log(`${i}`)
+    }
+}
+
+//end code
