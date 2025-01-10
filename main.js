99a1f5f54af552961f3a84187440b6ce7b120bc7
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-10 11:41:40 +0100

    add: workflow

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // start code
+
+// worklflow
+
+// 1. creare ciclo for
+//      - inizializzare variabile di ciclo "i"
+//      - condizione di avanzamento del ciclo
+//      - incremento della variabile "i"
+// 2. controllo se "i" è sia multiplo di 3 che di 5
+//      - se true -> stampo FizzBuzz
+//      - altrimenti se è solo multiplo di 3 -> stampo Fizz
+//      - altrimenti se è solo multiplo di 5 -> stampo Buzz
+//      - altrimenti stampo solo il num
